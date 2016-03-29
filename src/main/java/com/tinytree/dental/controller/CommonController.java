27379b24bd6b87cb36a32a3c907f3384f4f21b93
churcy <tinysafflower@gmail.com>
2016-03-29 16:08:04 +0800

    添加了controller文件项目可以正常访问

--- src/main/java/com/tinytree/dental/controller/CommonController.java
+++ src/main/java/com/tinytree/dental/controller/CommonController.java
@@ -1,9 +1,30 @@
 package com.tinytree.dental.controller;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 @Controller
 public class CommonController{
-  
+
+
+    @ResponseBody
+    @RequestMapping(value = "/common/systemTime",method = RequestMethod.GET)
+    public String getSystemTime(){
+        JSONObject jsonObject = new JSONObject();
+        try{
+            jsonObject.put("status","000000000");
+            long systemTime = new Date().getTime();
+            jsonObject.put("systemTime",systemTime);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
+    }
+
 
 }
